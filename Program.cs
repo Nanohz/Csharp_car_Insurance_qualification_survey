00726bdb5733ac10a950c87a67e3890288c7499d
Nanohz <38676579+Nanohz@users.noreply.github.com>
2018-09-22 16:21:31 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace C_sharp_Car_insurance_survey
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Approval Qoute For Care Insurance:");
+            Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int age_ = Convert.ToInt16(age);
+            Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI? True or False?");
+            string yesOrno = Console.ReadLine();
+            bool trueOrfalse = Convert.ToBoolean(yesOrno);
+            Console.ReadLine();
+            Console.WriteLine("Your answer is :" + trueOrfalse);
+            Console.ReadLine();
+            Console.WriteLine("If any, How many speeding tickets do you have?");
+            Console.ReadLine();
+            string ticketNumb = Console.ReadLine();
+            int ticketNumbConv = Convert.ToInt16(ticketNumb);
+            Console.ReadLine();
+            Console.WriteLine("Qualified? :");
+            Console.ReadLine();
+
+
+            if (age_ >= 15  && ticketNumbConv <= 3 && trueOrfalse == false)
+            
+            {
+
+                Console.WriteLine("Your qualification equals : TRUE!");
+
+            }
+            else {
+
+                Console.WriteLine("Sorry, But you do not qualify.");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
